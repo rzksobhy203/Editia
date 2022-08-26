fb830bb6ffd0930cb354d2b333437092793895a1
Sobhy Rzk <rzksobhy203@gmail.com>
2022-08-26 07:05:52 +0200

    add edpch.h & setup the logging system with spdlog

---
+++ src/log.cpp
@@ -0,0 +1,25 @@
+#include "edpch.h"
+#include "log.h"
+
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+namespace Editia
+{
+    std::shared_ptr<spdlog::logger> Log::s_Logger = nullptr;
+
+    void Log::init()
+    {
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Editia.log", true));
+
+        logSinks[0]->set_pattern("%^[%T] [%l] %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %v");
+
+        s_Logger = std::make_shared<spdlog::logger>("EDITIA", logSinks.begin(), logSinks.end());
+        spdlog::register_logger(s_Logger);
+        s_Logger->set_level(spdlog::level::trace);
+        s_Logger->flush_on(spdlog::level::trace);
+    }
+}
